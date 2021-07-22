03d2a1c406e63c4d30a20de7d5b00ed4963457ad
navdeep <navdepdhakar1@gmai.com>
2021-07-22 15:19:29 +0100

    changed all request responses to json only

--- req_handler/feed_requests/home_feed.js
+++ req_handler/feed_requests/home_feed.js
@@ -33,7 +33,24 @@ async function current_auth_check(url = '', auth_token) {
   }); 
   return response.json(); // parses JSON response into native JavaScript objects
   }
-
+  async function getData(url = '', req) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'GET', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers:{
+        'Content-Type': 'application/json',
+        'authorization':req.headers['authorization']
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      // body data type must match "Content-Type" header
+    }); 
+    return response.json(); // parses JSON response into native JavaScript objects
+    }
 
 api_comms_data = {
     req_path:"home_feed",
@@ -45,39 +62,26 @@ const namespace = "[home_feed]:";
 
 router.get('/home_feed', (req, res) =>{
     console.log(namespace + "request recieved");
-    
-    if(req.headers['authorization']!==undefined||null){
+    subsection = req.headers['req_path'];
+    if(req.headers['authorization']!==undefined||null||""){
       current_auth_check(current_auth_uri, req.headers['authorization'])
       .then(data => {
       console.log(data); // JSON data parsed by `data.json()` call
       });
           // Example POST method implementation:
-        async function getData(url = '') {
-        // Default options are marked with *
-        const response = await fetch(url, {
-          method: 'GET', // *GET, POST, PUT, DELETE, etc.
-          mode: 'cors', // no-cors, *cors, same-origin
-          cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-          credentials: 'same-origin', // include, *same-origin, omit
-          headers:{
-            'Content-Type': 'application/json',
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-          },
-          redirect: 'follow', // manual, *follow, error
-          referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-          // body data type must match "Content-Type" header
-        }); 
-        return response.text(); // parses JSON response into native JavaScript objects
-        }
-        subsection = req.headers['req_path'];
-        getData(proctor_home_uri + `/${subsection}`)
+
+        getData(proctor_home_uri + `/${subsection}`, req)
         .then(data => {
           res.send(data); // JSON data parsed by `data.json()` call
         });
     }
     else{
         console.log('generating random home page');
-        res.send("random home page");
+        
+        getData(proctor_home_uri + `/${subsection}`, req)
+        .then(data => {
+          res.send(data); // JSON data parsed by `data.json()` call
+        });
     }
 }); 
 module.exports = router;
